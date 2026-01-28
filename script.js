466ff596f17f00f9d89c7c27f025d95edc31eb55
~senpai <dhairaysenpai@gmail.com>
2026-01-28 20:13:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const questionContainer = document.querySelector(".question-container");
-const resultContainer = document.querySelector(".result-container.yes");
+const resultContainer = document.querySelector(".result-container");
 const gifResult = document.querySelector(".gif-result");
 const heartLoader = document.querySelector(".cssload-main");
 const yesBtn = document.querySelector(".js-yes-btn");
@@ -10,7 +10,7 @@ noBtn.addEventListener("mouseover", () => {
   const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
   const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
 
-  noBtn.style.left = `${newX}px`;
+  noBtn.style.left = `${newX}px';
   noBtn.style.top = `${newY}px`;
 });
 
@@ -26,3 +26,4 @@ yesBtn.addEventListener("click", () => {
     gifResult.play();
   }, 3000);
 });
+
